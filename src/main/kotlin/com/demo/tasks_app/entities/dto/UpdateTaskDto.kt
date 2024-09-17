8b2fefbb75d1f687481f6c3ea1eb00131b670770
Edwin Ndonga <ndonga.edwin@gmail.com>
2024-09-18 00:10:34 +0300

    add json property to update task dto

--- src/main/kotlin/com/demo/tasks_app/entities/dto/UpdateTaskDto.kt
+++ src/main/kotlin/com/demo/tasks_app/entities/dto/UpdateTaskDto.kt
@@ -2,13 +2,16 @@ package com.demo.tasks_app.entities.dto
 
 import com.demo.tasks_app.entities.Priority
 import com.demo.tasks_app.entities.ValueOfEnum
+import com.fasterxml.jackson.annotation.JsonProperty
 import jakarta.validation.constraints.NotBlank
 
 data class UpdateTaskDto(
     @NotBlank(message = "Task ID is required")
     val id: Long,
     val description: String?,
+    @JsonProperty("isReminderSet")
     val isReminderSet: Boolean?,
+    @JsonProperty("isTaskOpen")
     val isTaskOpen: Boolean?,
     @field:ValueOfEnum(enumClass = Priority::class, message = "")
     val priority: String?
